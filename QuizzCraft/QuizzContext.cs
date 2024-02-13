43223bc2934563ebd7a88890375269c661f54319
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-02-13 18:08:01 +0530

    feat: Database Connection, DbContext, AddUser method, ef and bcrypt packages added

---
+++ QuizzCraft/QuizzContext.cs
@@ -0,0 +1,20 @@
+﻿using QuizzCraft.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzCraft
+{
+    public class QuizzContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public QuizzContext() : base("QuizzConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<QuizzContext>());
+        }
+    }
+}
